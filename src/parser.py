946c7fdce237e302d432b118a0f0c614f5131547
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-15 18:54:08 +0300

    Добавлены методв для работы с файлом

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from abc import ABC, abstractmethod
+import os
 from datetime import datetime
 
 
@@ -103,7 +104,7 @@ class Vacancy:
                  responsibility: str = ""):
         self.vacancy_id = vacancy_id
         self.name = name
-        self.salary_from = salary_from if salary_from is not None else "Не указано"
+        self.salary_from = salary_from if salary_from is not None else 0
         self.salary_to = salary_to if salary_to is not None else "Не указано"
         self.currency = currency if currency is not None else ""
         self.url = url
@@ -120,28 +121,65 @@ class Vacancy:
                 f"Требования к соискателю - {self.requirement}\n"
                 f"Обязанности - {self.responsibility}\n")
 
-    def __ge__(self, other):
+    def __gt__(self, other):
         if self.salary_from and other.salary_from is not None:
-            return self.salary_from >= other.salary_from
+            return self.salary_from > other.salary_from
+
+    def __lt__(self, other):
+        if self.salary_from and other.salary_from is not None:
+            return self.salary_from < other.salary_from
 
 
 class WorkingWithFile(ABC):
 
     @abstractmethod
-    def reader(self):
+    def read_from_file(self):
         pass
 
     @abstractmethod
-    def writer(self):
+    def write_in_file(self, vacancy):
         pass
 
-    def filter(self):
+    @abstractmethod
+    def get_top_vacancy(self, top):
         pass
+
+    @abstractmethod
+    def deleter(self):
+        pass
+
+
+class Interaction(WorkingWithFile, ABC):
+    def __init__(self, file_name: str = "vacancy.json"):
+        self.file_name = file_name
+
+    def read_from_file(self):
+        with open(self.file_name, "r", encoding="utf-8") as file:
+            result = json.load(file)
+        return result
+
+    def write_in_file(self, vacancy):
+        with open(self.file_name, "w", encoding="utf-8") as file:
+            json.dump(vacancy, file, ensure_ascii=False, indent=4)
+
+    def get_top_vacancy(self, top):
+        data = self.read_from_file()
+        new_data = [Vacancy(**x) for x in data]
+        for i in range(len(new_data)):
+            for j in range(0, len(data)-1):
+                if new_data[j] > new_data[j+1]:
+                    new_data[j], new_data[j+1] = new_data[j+1], new_data[j]
+        new_data = new_data[::-1]
+        return new_data[:top]
 
     def deleter(self):
         pass
 
 
-# a = HeadHunterVacancies("python developer", "Волгоград")
-# c = [print(Vacancy(**vac)) for vac in a.vacancy]
-# s = Vacancy()
+a = HeadHunterVacancies("python developer", "Волгоград")
+# path = "/job_parser/file/vac.json"
+# with open(path, "w", encoding="utf-8") as file:
+#     json.dump(a.vacancy, file, ensure_ascii=False, indent=4)
+b = Interaction()
+k = b.get_top_vacancy(5)
+[print(x) for x in k]
