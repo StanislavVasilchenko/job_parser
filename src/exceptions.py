df3bc0dfb739fc63f756253b597bb42deb348a33
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-17 20:20:27 +0300

    Создан класс ошибки при сбои запроса к сайту

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,4 +1,4 @@
 class FailedConnection(Exception):
 
-    def __init__(self, *args, **kwargs ):
+    def __init__(self, *args, **kwargs):
         self.message = args[0] if args else "Не удачное подключение к сайту"
