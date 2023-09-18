45de3445770aeeaf3d5bb901f2e057f536208edb
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-18 21:59:56 +0300

    Добавлена проверка при пустом api_key

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,11 @@
 from src.parser import HeadHunterVacancies, SuperJobVacancies
 
 
-def site_selection(choice: int, key_words: str, city: str, api_key=None,):
-    if choice == 2 or 3 and api_key is None:
-        return
-    if choice == 1:
+def site_selection(choice: int, key_words: str, city: str, api_key):
+    if not api_key:
+        print("Введите ключ что бы воспользоваться сервисом Super Job")
+        quit()
+    elif choice == 1:
         hh_vacancies = HeadHunterVacancies(key_words=key_words, city=city)
         return hh_vacancies.vacancy
     elif choice == 2:
@@ -15,3 +16,7 @@ def site_selection(choice: int, key_words: str, city: str, api_key=None,):
         hh_vacancies = HeadHunterVacancies(key_words=key_words, city=city)
         sj_vacancies = SuperJobVacancies(api_key, key_words, city)
         return hh_vacancies + sj_vacancies
+
+
+def user_interaction():
+    pass
