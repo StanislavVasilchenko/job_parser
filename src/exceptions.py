4b3fccef3b2585d015505fdba3fc10581433f306
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-09-18 15:47:35 +0300

    Добавлен класс исключения, если город не найден CityNotFound

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,3 +2,9 @@ class FailedConnection(Exception):
 
     def __init__(self, *args, **kwargs):
         self.message = args[0] if args else "Не удачное подключение к сайту"
+
+
+class CityNotFound(Exception):
+
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else "CityNotFound"
